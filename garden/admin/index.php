eb5b13a2381d3232b44e789208357d0ae310297c
Sarunas Slekys <52355769+SarunasPersonal@users.noreply.github.com>
2024-06-04 02:26:19 +0100

    Initial commit

---
+++ garden/admin/index.php
@@ -0,0 +1,40 @@
+<?php include("../config/constants.php") ?>
+<html>
+
+<head>
+    <title>Broadleigh Gardens - Home page</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+
+<body>
+    <!-- Menu Starts -->
+    <div class="menu text-center">
+        <div class="wrapper">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="manage-admin.php">Admin</a></li>
+                <li><a href="manage-category.php">Category</a></li>
+                <li><a href="manage-food.php">Flowers</a></li>
+                <li><a href="manage-order.php">Order</a></li>
+            </ul>
+        </div>
+    </div>
+<!-- Menu Ends -->
+<!-- Main Content Starts -->
+<!-- MAIN CONTENT ENDS -->
+
+<!-- Footer Section Starts -->
+<div class="footer">
+    <div class="wrapper">
+        <div class="text-center">
+            <p>&copy; 2024 Bradleigh Gardens. Developed By <a href="#">Sarunas Slekys</a></p>
+        </div>
+    </div>
+</div>
+</body>
+
+</html>
+<!-- FOOTER ENDS HERE -->
+
+
+    
---
+++ garden/admin/partials/menu.php
@@ -0,0 +1,21 @@
+<?php include("../config/constants.php") ?>
+<html>
+
+<head>
+    <title>Bradleigh Gardens - Home page</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+
+<body>
+    <!-- Menu Starts -->
+    <div class="menu text-center">
+        <div class="wrapper">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="manage-admin.php">Admin</a></li>
+                <li><a href="manage-category.php">Category</a></li>
+                <li><a href="manage-food.php">Food</a></li>
+                <li><a href="manage-order.php">Order</a></li>
+            </ul>
+        </div>
+    </div>
